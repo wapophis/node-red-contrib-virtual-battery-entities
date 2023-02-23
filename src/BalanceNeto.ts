b5b3bcf0b201b17d4ddbd9cd7b6a9008b0f8e2f6
Wapophis <luis.martinez.exts@juntadeandalucia.es>
2023-02-24 00:36:11 +0100

    Added autoconsolidation and better control, when adding slots.

--- src/BalanceNeto.ts
+++ src/BalanceNeto.ts
@@ -44,4 +44,124 @@ export class BalanceNeto{
         this.endTime=this.startTime.plusMinutes(durationInMinutes);
         return this;
     }
+
+    addBatterySlot(slot:BatterySlot){
+        var slotStart=LocalDateTime.parse(slot.readTimeStamp.toString());
+        
+        if(slotStart.isBefore(this.startTime)){
+            throw "Slot time is before than this slot start time";
+        }
+        if(slot.getLength()<0){
+            throw "Discarded slot, because no lenght defined";
+        }
+        if(slot.getLength()>this.duration.toMillis()){
+            throw "Cannot add slots with duration "+slot.getLength()+"ms greater than the net balance duration "+this.duration.toMillis()+"ms";
+        }
+
+        if(this.isConsolidable()===true){
+            throw "Cannot add slots the net balance is consolidable."
+        }
+
+        if(slot.consumedInWatsH===undefined || isNaN(slot.consumedInWatsH)){
+            throw "Error in slot data, consumedInWatsH undefined";
+        }
+
+        if(slot.feededInWatsH===undefined || isNaN(slot.feededInWatsH) ){
+            throw "Error in slot data, feededInWatsH undefined";
+        }
+
+        if(slot.producedInWatsH===undefined || isNaN(slot.producedInWatsH) ){
+            throw "Error in slot data, producedInWatsH undefined";
+        }
+
+        this.batterySlots.push(slot);
+        this._autoConsolidate();
+        
+    }
+
+    getProduced(){
+        let count=0;
+        this.batterySlots.forEach(function(item:BatterySlot){
+            let slotsInHour=(60*60*1000)/item.getLength();
+            count+=item.producedInWatsH/slotsInHour;
+            if(isNaN(count)){
+                console.log(item);
+            }
+        });
+        return count;
+    }
+    getFeeded(){
+        let count=0;
+        this.batterySlots.forEach(function(item){
+            let slotsInHour=(60*60*1000)/item.getLength();
+            count+=item.feededInWatsH/slotsInHour;
+            if(isNaN(count)){
+                console.log(item);
+                return 0;
+            }
+        });
+        return count;
+    }
+    getConsumed(){
+        let count=0;
+        this.batterySlots.forEach(function(item){
+            let slotsInHour=(60*60*1000)/item.getLength();
+            count+=item.consumedInWatsH/slotsInHour;
+            if(isNaN(count)){
+                console.log(item);
+            }
+        });
+        return count;
+    }
+
+    
+    get(){
+        return {
+            balanceNetoHorario:{
+            feeded:this.getFeeded(),
+            consumed:this.getConsumed(),
+            produced:this.getProduced(),
+            startAt:this.startTime,
+            endAt:this.endTime,
+            isConsolidable:this.isConsolidable(),
+            batterySlots:this.batterySlots,
+            length:this.batterySlots.length,
+            startTime:this.startTime,
+            endTime:this.endTime
+            }
+        }
+    }
+    
+    getEndTime():LocalDateTime {
+        return LocalDateTime.parse(this.endTime.toString());
+    }
+
+    getStartTime():LocalDateTime{
+        return LocalDateTime.parse(this.startTime.toString());
+    }
+
+
+    isConsolidable(){
+        return this.consolidable;
+    }
+
+    _autoConsolidate(){
+        /*let slotsTotalDuration=Duration.ofMinutes(0);
+        for(let i=0,j=i+1;i<this.batterySlots.length&&j<this.batterySlots.length;i++,j=i+1){
+                this.batterySlots[i].calcLenght(this.batterySlots[j]);
+                slotsTotalDuration=slotsTotalDuration.plusMillis(this.batterySlots[i].getLength());
+                console.log(slotsTotalDuration.toMillis());
+        }
+        if(slotsTotalDuration.compareTo(this.duration)>0){
+            this.consolidable=true;
+        }*/
+        this.consolidable=this.endTime.isBefore(this.batterySlots[this.batterySlots.length-1].readTimeStamp);
+    }
+
+    
+    of(input:any,type:string){
+        if(type="e-distribucion"){
+            
+        }
+    }
 }
